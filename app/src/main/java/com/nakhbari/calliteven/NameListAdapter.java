c5c403c2e7f6c733aedb8aee09bac33573b26b3c
Nima Laptop <nnakhbari@gmail.com>
2015-02-16 00:26:06 -0500

    simplified namelist ListView TODO: Add divider Get Avatar library Fixing EntryList

--- app/src/main/java/com/nakhbari/calliteven/NameListAdapter.java
+++ app/src/main/java/com/nakhbari/calliteven/NameListAdapter.java
@@ -90,39 +90,19 @@ public class NameListAdapter extends ArrayAdapter<NameListItem> {
             LayoutInflater inflater = (LayoutInflater) getContext()
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = inflater.inflate(R.layout.row_name_list, parent, false);
-            holder.name = (TextView) view.findViewById(R.id.nameText);
-            holder.latestEntries = (TextView) view
-                    .findViewById(R.id.tvLatestItems);
-            holder.divLine2
-                    = (TextView) view.findViewById(R.id.divLine2);
-            holder.owesWho = (TextView) view
-                    .findViewById(R.id.tvOwing);
-            holder.balance = (TextView) view
-                    .findViewById(R.id.tvBalance);
-            holder.circleImage = (ImageView) view
-                    .findViewById(R.id.nameItemCircle);
+            holder.name = (TextView) view.findViewById(R.id.name_row_name);
+            holder.balance = (TextView) view.findViewById(R.id.name_row_balance);
 
             // set fonts
-            Typeface tf = Typeface.createFromAsset(getContext().getAssets(),
-                    "fonts/Roboto-Light.ttf");
-            Typeface tfItalic = Typeface.createFromAsset(getContext()
-                    .getAssets(), "fonts/Roboto-LightItalic.ttf");
+            Typeface robotoReg = Typeface.createFromAsset(getContext().getAssets(),
+                    "fonts/Roboto-Regular.ttf");
 
-            holder.name.setTypeface(tf);
-            holder.latestEntries.setTypeface(tfItalic);
-
-            holder.balance.setTypeface(tf);
-            holder.owesWho.setTypeface(tf);
-
+            holder.name.setTypeface(robotoReg);
+            holder.balance.setTypeface(robotoReg);
             view.setTag(holder);
+
         } else {
             holder = (ViewHolder) view.getTag();
-        }
-
-        // Sliding in animation if its not the same position and the item isn't
-        // clicked
-        if (position != holder.lastPosition && mSelection.get(position) == null) {
-            holder.lastPosition = position;
         }
 
 		/*
@@ -134,92 +114,15 @@ public class NameListAdapter extends ArrayAdapter<NameListItem> {
 		 * Therefore, i refers to the current Item object.
 		 */
 
-        if (mSelection.get(position) != null) {
-            view.setBackgroundResource(R.color.list_item_long_press); // this is
-            // a selected position so make it dark blue
-        } else {
-            view.setBackgroundResource(android.R.color.transparent);
-        }
 
         NameListItem i = objects.get(position);
 
         if (i != null) {
 
             // Check to see if each individual name is null
-            if (holder.name != null) {
-                holder.name.setText(i.getName());
-            }
-
-            double balance = i.getBalance();
-
-            // Manage what happens with the balance
-            if (holder.balance != null) {
-
-                if (balance < 0) {
-
-                    holder.balance.setVisibility(View.VISIBLE);
-                    holder.owesWho.setText("Is Owed");
-                    holder.balance.setText(
-                            currency
-                                    + FormatDoubleToString(Math
-                                    .abs(balance)));
-
-                } else if (balance > 0) {
-
-                    holder.balance.setVisibility(View.VISIBLE);
-                    holder.owesWho.setText("Owes You");
-                    holder.balance.setText(
-                            currency
-                                    + FormatDoubleToString(Math
-                                    .abs(balance)));
-
-                } else {
-                    // No Money Owed
-                    holder.balance.setVisibility(View.GONE);
-
-                    if (i.getListOfLatestEntries().isEmpty()) {
-                        // Nothing is owed
-
-                        holder.owesWho.setText(
-                                R.string.no_balance);
-                    } else {
-                        holder.owesWho.setText("Object Lent");
-                    }
-                }
-            }
-
-
-            // Manage Latest Items
-            if (i.getListOfLatestEntries().isEmpty()) {
-
-                holder.latestEntries.setVisibility(View.GONE);
-                holder.divLine2.setVisibility(View.GONE);
-                holder.owesWho.setTextColor(
-                        getContext().getResources().getColor(R.color.green));
-                holder.circleImage.setImageResource(R.drawable.ic_check);
-                holder.circleImage.setBackgroundResource(
-                        R.drawable.green_circle);
-            } else {
-
-                holder.latestEntries.setText(i.getListOfLatestEntries()
-                        .getList());
-                holder.latestEntries.setVisibility(View.VISIBLE);
-                holder.divLine2.setVisibility(View.VISIBLE);
-
-                if (balance == 0) {
-
-                    holder.owesWho.setTextColor(
-                            getContext().getResources().getColor(
-                                    R.color.actionbar_background));
-                    holder.circleImage.setImageResource(
-                            R.drawable.ic_arrow_forward);
-                    holder.circleImage.setBackgroundResource(
-                            R.drawable.blue_circle);
-                }
-            }
-
+            holder.name.setText(i.getName());
+            holder.balance.setText(currency + FormatDoubleToString(i.getBalance()));
         }
-
 
         // Return the view to the activity
         return view;
